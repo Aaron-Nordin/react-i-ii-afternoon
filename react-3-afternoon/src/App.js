ed79acbcc6f1d198cb339bfe95a19ab3936da2b2
Aaron <nordin.aaron@gmail.com>
2019-07-10 16:48:37 -0600

    stylz

--- react-3-afternoon/src/App.js
+++ react-3-afternoon/src/App.js
@@ -32,36 +32,42 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">Home</header>
-        <div className="infoCard">
-          <div className="num">{`${this.handle25()}`}</div>
-          <div className="name">
-            {`${data[this.state.currentIndex].name.first} ${
-              data[this.state.currentIndex].name.last
-            }`}
-          </div>
-          <div className="from">
-            {`From: ${data[this.state.currentIndex].city}, ${
-              data[this.state.currentIndex].country
-            }`}
-          </div>
-          <div className="employer">{`Employer: ${
-            data[this.state.currentIndex].employer
-          }`}</div>
-          <div className="faveMovHead">Favorite Movies:</div>
-          <div className="faveMovies">
-            <div className="one">{`1. ${
-              data[this.state.currentIndex].favoriteMovies[0]
+        <div className="background">
+          <div className="infoCard">
+            <div className="num">{`${this.handle25()}`}</div>
+            <div className="name">
+              {`${data[this.state.currentIndex].name.first} ${
+                data[this.state.currentIndex].name.last
+              }`}
+            </div>
+            <div className="from">
+              {`From: ${data[this.state.currentIndex].city}, ${
+                data[this.state.currentIndex].country
+              }`}
+            </div>
+            <div className="employer">{`Employer: ${
+              data[this.state.currentIndex].employer
             }`}</div>
-            <div className="two">{`2. ${
-              data[this.state.currentIndex].favoriteMovies[1]
-            }`}</div>
-            <div className="three">{`3. ${
-              data[this.state.currentIndex].favoriteMovies[2]
-            }`}</div>
+            <div className="faveMovHead">Favorite Movies:</div>
+            <div className="faveMovies">
+              <div className="one">{`1. ${
+                data[this.state.currentIndex].favoriteMovies[0]
+              }`}</div>
+              <div className="two">{`2. ${
+                data[this.state.currentIndex].favoriteMovies[1]
+              }`}</div>
+              <div className="three">{`3. ${
+                data[this.state.currentIndex].favoriteMovies[2]
+              }`}</div>
+            </div>
+            <div>
+              <div className="prevNext">
+                <Previous indexFn={this.handlePrevious} />
+                <Next indexFn={this.handleNext} />
+              </div>
+            </div>
           </div>
         </div>
-        <Previous indexFn={this.handlePrevious} />
-        <Next indexFn={this.handleNext} />
       </div>
     );
   }
